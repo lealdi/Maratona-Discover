84a72b77255e6748ee76fc45389f96096563c753
Diego Leal <diego_ldxsbc@hotmail.com>
2021-02-19 19:34:19 -0300

    updates index and script

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ const Modal = {
     }
 }
 
+const transactions = [
+    {
+        id:1,
+        description:"Luz",
+        amount: -50000,
+        date: "23/01/2021",
+    },
+    {
+        id:2,
+        description:"Website",
+        amount: 500000,
+        date: "23/01/2021",
+    },
+    {
+        id:3,
+        description:"Internet",
+        amount: -20000,
+        date: "23/01/2021",
+    },
+]
+
+// Eu preciso somar as entradas
+// depois eu preciso somar as saidas e
+// remover das entradas o valor das saidas
+// assim, eu terei o total
+
+const Transaction = {
+    incomes() {
+        // Somar as entradas
+    },
+    expenses() {
+        // Somar as saídas
+    },
+    total() {
+        // Entradas - saidas
+    }
+
+}
